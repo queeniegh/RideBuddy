e9ccb83386cafbebef69c61d12a9d93019f3681a
Mike Hinds <mthinds@umich.edu>
2018-11-29 20:46:03 -0500

    Added activities

---
+++ app/src/main/java/com/example/queenie/ridebuddy/RideConfirmationActivity.java
@@ -0,0 +1,13 @@
+package com.example.queenie.ridebuddy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RideConfirmationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_confirmation);
+    }
+}
