710275ff8814d783f0e78c61cb44fa11bd94dc97
mbadugha <mbadugha@umich.edu>
2018-12-03 12:16:59 -0500

    Buttons on landing page

---
+++ app/src/main/java/com/example/queenie/ridebuddy/RequestCompleteActivity.java
@@ -0,0 +1,13 @@
+package com.example.queenie.ridebuddy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RequestCompleteActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_complete);
+    }
+}
