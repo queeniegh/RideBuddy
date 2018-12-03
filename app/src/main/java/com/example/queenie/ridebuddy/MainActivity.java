910d4d996936102286002897ca60022b1a0c242e
Mike Hinds <mthinds@umich.edu>
2018-12-03 12:29:00 -0500

    Mike in Class

--- app/src/main/java/com/example/queenie/ridebuddy/MainActivity.java
+++ app/src/main/java/com/example/queenie/ridebuddy/MainActivity.java
@@ -1,13 +1,81 @@
 package com.example.queenie.ridebuddy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends Activity implements View.OnClickListener {
+
+    Button buttonRegister, buttonLogin;
+    EditText editTextEmail, editTextPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        buttonRegister = findViewById(R.id.buttonRegister);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        buttonLogin.setOnClickListener(this);
+        buttonRegister.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonRegister) {
+            mAuth.createUserWithEmailAndPassword(editTextEmail.getText().toString(), editTextPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(LoginActivity.this, "Registration Successful - Welcome!", Toast.LENGTH_SHORT).show();
+                                //Intent intentHome = new Intent(MainActivity.this,HomeActivity.class) ;
+                                //startActivity(intentHome);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(LoginActivity.this, "Registration Failed - Try Again", Toast.LENGTH_SHORT).show();
+                            }
+                            //...
+                        }
+                    });
+        } else if (v == buttonLogin)
+
+        {
+            mAuth.signInWithEmailAndPassword(editTextEmail.getText().toString(), editTextPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                Intent intentProfile = new Intent(LoginActivity.this, LandingActivity.class);
+                                startActivity(intentProfile);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(LoginActivity.this, "Access Denied", Toast.LENGTH_SHORT).show();
+                            }
+                            // ...
+                        }
+                    });
+        }
     }
 }
