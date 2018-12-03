b74716cf6e2589af449c19ea46c9d4b66bbd67c2
mbadugha <mbadugha@umich.edu>
2018-12-03 12:32:07 -0500

    Buttons on landing page

--- app/src/main/java/com/example/queenie/ridebuddy/LandingPageActivity.java
+++ app/src/main/java/com/example/queenie/ridebuddy/LandingPageActivity.java
@@ -1,26 +1,44 @@
 package com.example.queenie.ridebuddy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class LandingPageActivity extends Activity implements View.OnClickListener {
 
+    Button buttonStatus;
+    Button buttonRequest;
+    Button buttonEdit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
 
-        Button buttonStatus;
-        Button buttonRequest;
-        Button buttonEdit;
 
+        buttonEdit= findViewById(R.id.buttonEdit);
+        buttonRequest = findViewById(R.id.buttonRequest);
+        buttonStatus = findViewById(R.id.buttonStatus);
+
+        buttonEdit.setOnClickListener(this);
+        buttonRequest.setOnClickListener(this);
+        buttonStatus.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        
+
+        if (v == buttonEdit) {
+            Intent intentEdit = new Intent(this, EditProfileActivity.class);
+            startActivity(intentEdit);
+        }
+        if (v == buttonRequest) {
+            Intent intentRequest = new Intent(this, RideRequest.class );
+            startActivity(intentRequest);
+        }
+
     }
 }
