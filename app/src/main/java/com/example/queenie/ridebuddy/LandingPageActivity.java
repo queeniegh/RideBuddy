d0b1590d525472ab34e5ae3179a30e2ef5686c86
mbadugha <mbadugha@umich.edu>
2018-12-03 12:15:24 -0500

    Profile created for class

--- app/src/main/java/com/example/queenie/ridebuddy/LandingPageActivity.java
+++ app/src/main/java/com/example/queenie/ridebuddy/LandingPageActivity.java
@@ -2,6 +2,7 @@ package com.example.queenie.ridebuddy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LandingPageActivity extends Activity {
 
@@ -9,5 +10,9 @@ public class LandingPageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        Button buttonStatus;
+        Button buttonRequest;
+        Button buttonEdit;
     }
 }
