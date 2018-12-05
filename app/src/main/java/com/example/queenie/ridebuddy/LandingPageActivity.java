fc8fba502d470525c926ec4c443646502a9dde31
queeniegh <queenieb@umich.edu>
2018-12-05 11:24:38 -0500

    commit

--- app/src/main/java/com/example/queenie/ridebuddy/LandingPageActivity.java
+++ app/src/main/java/com/example/queenie/ridebuddy/LandingPageActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LandingPageActivity extends Activity implements View.OnClickListener {
 
@@ -34,14 +35,17 @@ public class LandingPageActivity extends Activity implements View.OnClickListene
         if (v == buttonEdit) {
             Intent intentEdit = new Intent(this, EditProfileActivity.class);
             startActivity(intentEdit);
-        }
-        if (v == buttonRequest) {
+        } else if (v == buttonRequest) {
             Intent intentRequest = new Intent(this, RideRequest.class );
             startActivity(intentRequest);
-        }
-        else if (v == buttonStatus){
-            Intent intentStatus = new Intent(this,RideStatusActivity.class );
-            startActivity(intentStatus);
+
+
+        } else if (v == buttonStatus){
+           // Intent intentStatus = new Intent(this,RideStatusActivity.class );
+           // startActivity(intentStatus);
+
+            Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+
         }
 
     }
